475b9ed368d76da5f678ef3379e28d8d2cbdf041
Raymond MASSON <ray@Raymonds-MacBook-Pro.local>
2017-08-25 18:45:42 +0200

    async writes in redux

--- src/components/course/CoursesPage.js
+++ src/components/course/CoursesPage.js
@@ -1,10 +1,17 @@
 import React, {PropTypes} from 'react';
 import {connect} from 'react-redux';
 import CoursesList from './CoursesList';
+import {browserHistory} from 'react-router';
 
 class CoursesPage extends React.Component {
   constructor(props, context) {
     super(props, context);
+
+    this.redirectToAddCoursePage = this.redirectToAddCoursePage.bind(this);
+  }
+
+  redirectToAddCoursePage() {
+    browserHistory.push('/course');
   }
 
   render() {
@@ -12,6 +19,11 @@ class CoursesPage extends React.Component {
     return (
       <div>
         <h1>Courses</h1>
+        <input
+          type="submit"
+          value="Add Course"
+          className="btn btn-primary"
+          onClick={this.redirectToAddCoursePage}/>
         <CoursesList courses={courses}/>
       </div>
     );
